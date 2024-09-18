2ee0884a5971c702ff2565af17a8a71f3812f207
Paulo Everton <120573680+PauloEverton05@users.noreply.github.com>
2024-09-18 19:48:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,65 +4,151 @@
 
 import tkinter as tk
 from tkinter import messagebox
+import customtkinter as ctk
 import mysql.connector
+from datetime import datetime
 
-def create_database():
-    cnx = mysql.connector.connect(
-        host='######',
-        user='#####',
-        password='####',
-        database='#####'
+def conectar_db():
+    return mysql.connector.connect(
+        host='localhost',
+        user='######',
+        password='#######',
+        database='academia'
     )
-    return cnx
 
-def create_table():
-    cnx = create_database()
-    cursor = cnx.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS clientes (
-                    id INT AUTO_INCREMENT PRIMARY KEY,
-                    nome VARCHAR(100) NOT NULL,
-                    idade INT NOT NULL
-                )''')
-    cnx.commit()
-    cnx.close()
+def add_aluno():
+    nome = entry_nome.get()
+    data_nascimento = entry_data_nascimento.get()
+    cpf = entry_cpf.get()
+    email = entry_email.get()
 
-def insert_client():
-    name = entry_name.get()
-    age = entry_age.get()
+    if not nome or not data_nascimento or not cpf or not email:
+        messagebox.showerror("Erro!", "Preencha todos os campos.")
+        return
     
-    if name and age:
-        try:
-            cnx = create_database()
-            cursor = cnx.cursor()
-            sql = 'INSERT INTO clientes (nome, idade) VALUES (%s, %s)'
-            val = (name, age)
-            cursor.execute(sql, val)
-            cnx.commit()
-            cnx.close()
-            messagebox.showinfo('Sucesso!', 'Cliente inserido com sucesso.')
-        except mysql.connector.Error as e:
-            messagebox.showerror('Erro!', f'Erro ao inserir cliente: {e}')
-    else:
-        messagebox.showerror('Erro!', 'Por favor, preencha todos os campos.')
+    try:
+        data_nascimento_formatada = datetime.strptime(data_nascimento, "%Y-%m-%d").date()
+    except ValueError:
+        messagebox.showerror("Erro!", "Data de nascimento inválida. Use o formato ano-mês-dia (ex: 1998-08-25).")
+        return
 
-#criar a interface gráfica
-root = tk.Tk()
-root.title('Sistema cadastro clientes')
+    conn = conectar_db()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(
+            'INSERT INTO alunos (nome, data_nascimento, cpf, email) VALUES (%s, %s, %s, %s)', 
+            (nome, data_nascimento_formatada, cpf, email)
+        )
+        conn.commit()
+        messagebox.showinfo("Sucesso", "Aluno adicionado com sucesso")
+        entry_nome.delete(0, tk.END)
+        entry_data_nascimento.delete(0, tk.END)
+        entry_cpf.delete(0, tk.END)
+        entry_email.delete(0, tk.END)
+        listar_alunos()
+    except mysql.connector.Error as err:
+        messagebox.showerror("Erro!", f"Erro ao adicionar aluno: {err}")
+    finally:
+        conn.close()
 
-#criar os widgets
-label_name = tk.Label(root, text='Nome:')
-label_name.grid(row=0, column=0, padx=30, pady=30)
-entry_name = tk.Entry(root)
-entry_name.grid(row=0, column=1, padx=30, pady=30)
+def excluir_aluno():
+    id = entry_id.get()
 
-label_age = tk.Label(root, text='Idade:')
-label_age.grid(row=1, column=0, padx=30, pady=30)
-entry_age = tk.Entry(root)
-entry_age.grid(row=1, column=1, padx=30, pady=30)
+    if not id:
+        messagebox.showerror("Erro!", "É necessário o ID para excluir!")
+        return
 
-btn_insert = tk.Button(root, text='ㅤㅤㅤInserirㅤㅤㅤ', command=insert_client)
-btn_insert.grid(row=2, column=0, columnspan=2, padx=30, pady=30)
-create_table()
+    conn = conectar_db()
+    cursor = conn.cursor()
+    try:
+        cursor.execute('DELETE FROM alunos WHERE id = %s', (id,))
+        conn.commit()
+        messagebox.showinfo("Sucesso", "Aluno deletado com sucesso")
+        entry_id.delete(0, tk.END)
+        listar_alunos()
+    except mysql.connector.Error as err:
+        messagebox.showerror("Erro!", f"Erro ao deletar aluno: {err}")
+    finally:
+        conn.close()
 
-#iniciar a interface gráfica
-root.mainloop()
+def proc_aluno():
+    nome = entry_nome.get()
+
+    conn = conectar_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM alunos WHERE nome LIKE %s', ('%' + nome + '%',))
+    resultados = cursor.fetchall()
+    conn.close()
+
+    listbox.delete(0, tk.END)
+    for resultado in resultados:
+        listbox.insert(tk.END, str(resultado))
+
+def listar_alunos():
+    conn = conectar_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM alunos')
+    resultados = cursor.fetchall()
+    conn.close()
+
+    listbox.delete(0, tk.END)
+    for resultado in resultados:
+        aluno_info = list(resultado)
+        aluno_info[2] = aluno_info[2].strftime("%Y-%m-%d")
+        listbox.insert(tk.END, str(tuple(aluno_info)))
+
+app = ctk.CTk()
+app.title("Cadastro de Alunos")
+app.geometry("430x560")
+
+frame = ctk.CTkFrame(app)
+frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+
+form_layout = ctk.CTkFrame(frame)
+form_layout.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+
+label_nome = ctk.CTkLabel(form_layout, text="Nome")
+label_nome.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+entry_nome = ctk.CTkEntry(form_layout)
+entry_nome.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
+
+label_data_nascimento = ctk.CTkLabel(form_layout, text="Data de Nascimento (ano-mês-dia)")
+label_data_nascimento.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+entry_data_nascimento = ctk.CTkEntry(form_layout)
+entry_data_nascimento.grid(row=1, column=1, padx=5, pady=5, sticky="ew")
+
+label_cpf = ctk.CTkLabel(form_layout, text="CPF (apenas números)")
+label_cpf.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+entry_cpf = ctk.CTkEntry(form_layout)
+entry_cpf.grid(row=2, column=1, padx=5, pady=5, sticky="ew")
+
+label_email = ctk.CTkLabel(form_layout, text="Email")
+label_email.grid(row=3, column=0, padx=5, pady=5, sticky="w")
+entry_email = ctk.CTkEntry(form_layout)
+entry_email.grid(row=3, column=1, padx=5, pady=5, sticky="ew")
+
+btn_frame = ctk.CTkFrame(form_layout)
+btn_frame.grid(row=4, column=0, columnspan=2, padx=5, pady=5, sticky="ew")
+
+btn_add = ctk.CTkButton(btn_frame, text="Adicionar", command=add_aluno, fg_color="#2c7833")
+btn_add.grid(row=0, column=0, padx=20, pady=10, sticky="ew")
+
+btn_proc = ctk.CTkButton(btn_frame, text="Buscar", command=proc_aluno, fg_color="#2c7833")
+btn_proc.grid(row=0, column=1, padx=20, pady=10, sticky="ew")
+
+label_id = ctk.CTkLabel(form_layout, text="ID para Deletar")
+label_id.grid(row=5, column=0, padx=5, pady=5, sticky="w")
+entry_id = ctk.CTkEntry(form_layout)
+entry_id.grid(row=5, column=1, padx=5, pady=5, sticky="ew")
+
+btn_del = ctk.CTkButton(form_layout, text="Deletar", command=excluir_aluno, fg_color="#782c2c")
+btn_del.grid(row=6, column=0, columnspan=2, padx=5, pady=10, sticky="ew")
+
+listbox = tk.Listbox(frame, height=10)
+listbox.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+
+frame.rowconfigure(1, weight=1)
+frame.columnconfigure(0, weight=1)
+
+listar_alunos()
+app.mainloop()
